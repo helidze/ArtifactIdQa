27beba5648640656e1496c38419a7342f7dd90e8
helidze <helidze@gmail.com>
2018-01-22 16:54:25 +0200

    create project

---
+++ src/main/java/GroupIdQa/Factorial.java
@@ -0,0 +1,24 @@
+package GroupIdQa;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        System.out.println("Факториал числа: " + factorial.factorialOfNumber());
+    }
+
+    public  int factorialOfNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int x = scanner.nextInt();
+
+        int fact = 1;
+        for (int i = 1; i <=x ; i++) {
+            fact*=i;
+
+        }
+        return fact;
+    }
+}
