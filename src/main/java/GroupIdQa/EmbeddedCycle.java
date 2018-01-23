d6f7a0e07e97e4391ec576862724f108be735a5e
helidze <helidze@gmail.com>
2018-01-23 10:43:44 +0200

    Fixing issue in EmbeddedCycle

--- src/main/java/GroupIdQa/EmbeddedCycle.java
+++ src/main/java/GroupIdQa/EmbeddedCycle.java
@@ -2,11 +2,11 @@ package GroupIdQa;
 
 public class EmbeddedCycle {
     public static void main(String[] args) {
-        zeroTriangle(2);
+        zeroTriangle(5);
     }
 
     public static void zeroTriangle(int count) {
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i <= count; i++) {
             for (int j = 0; j < i; j++)
                 System.out.print("0");
             System.out.println();
