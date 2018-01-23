91df2a6b3a5c87737fe79e5e243b9272023e44e1
helidze <helidze@gmail.com>
2018-01-23 11:26:23 +0200

    Fixing triangle

--- src/main/java/GroupIdQa/TriangleSides.java
+++ src/main/java/GroupIdQa/TriangleSides.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class TriangleSides {
     public static void main(String[] args) {
         TriangleSides triangleSides = new TriangleSides();
-        System.out.println("Это треугольник? " + triangleSides.sidesChecker());
-    }
-    public boolean sidesChecker(){
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ввести сторону A: ");
@@ -17,15 +14,20 @@ public class TriangleSides {
         System.out.print("Ввести сторону C: ");
         int c = scanner.nextInt();
 
-        if (a > b+c || b > a+c || c > a+b){
-            System.out.println("Это не треугольник");
-            if (a == 0 || b == 0 || c == 0){
-                System.out.println("Сторона не может быть 0");
-            }
-        }
-        else
-        return true;
+        System.out.println("Это треугольник? " + triangleSides.sidesChecker(a, b, c));
+    }
 
-        return sidesChecker(); // повторяем метод до того момента пока не введены правильные стороны треугольника
+    private boolean sidesChecker(int a, int b, int c) {
+        if (a == 0 || b == 0 || c == 0) {
+            System.out.println("Сторона не может быть 0");
+            return false;
+        }
+
+        if (a > b + c || b > a + c || c > a + b) {
+            System.out.println("Это не треугольник");
+            return false;
+        }
+
+        return true;
     }
 }
