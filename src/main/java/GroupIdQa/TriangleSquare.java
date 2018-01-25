7c99e16335f1de926ffc08bd965defebc0ff8bb0
helidze <helidze@gmail.com>
2018-01-25 13:21:19 +0200

    TriangleSquare

---
+++ src/main/java/GroupIdQa/TriangleSquare.java
@@ -0,0 +1,24 @@
+package GroupIdQa;
+
+import java.util.Scanner;
+
+public class TriangleSquare {
+    public static void main(String[] args) {
+        TriangleSquare sqr = new TriangleSquare();
+        sqr.getTriangleSquare();
+    }
+
+    protected void getTriangleSquare() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите сторону правильного треугольника: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите высоту правильного треугольника: ");
+        int h = scanner.nextInt();
+
+        double S = 0.5 * a * h;
+        System.out.print("\nПлощадь треугольника = " + S);
+
+
+    }
+
+}
