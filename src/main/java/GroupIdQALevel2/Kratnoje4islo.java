88a97b6b01a90ddf8d00fedea23e61ba924325f1
helidze <helidze@gmail.com>
2018-01-26 17:35:50 +0200

    Kratnoje

--- src/main/java/GroupIdQALevel2/Kratnoje4islo.java
+++ src/main/java/GroupIdQALevel2/Kratnoje4islo.java
@@ -7,19 +7,19 @@ public class Kratnoje4islo {
     public static void main(String[] args) {
         Kratnoje4islo kratnoje4islo = new Kratnoje4islo();
         kratnoje4islo.getKratnoje4islo();
-
-
     }
-    private void getKratnoje4islo(){
-        int [] arr = new int[10];
-        int g = 1;
+
+    private void getKratnoje4islo() {
+        int n = 20;
+        int[] arr = new int[n];
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите кратное число");
         int kratnoje = scanner.nextInt();
-        for (int i = 1; i < arr.length ; i=i+13) {
-            arr[i] = arr [i] + i;
-            if (arr[i] % kratnoje == 0){
-                g*=arr[i];
-                System.out.println(g);
+        int i = 0;
+        for (i = 0; i < n; i++) {
+            arr[i] = i;
+            if (arr[i] % kratnoje == 0) {
+                System.out.println(arr[i]);
             }
         }
 
