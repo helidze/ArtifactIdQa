444fdb12c2716b02efba638e7e56397c9c865392
helidze <helidze@gmail.com>
2018-01-29 15:30:01 +0200

    AllTasksInOneTest

--- src/main/java/GroupIdQALevel2/AllTasksInOneL2.java
+++ src/main/java/GroupIdQALevel2/AllTasksInOneL2.java
@@ -1,42 +1,28 @@
 package GroupIdQALevel2;
 
-import org.testng.annotations.Test;
-
-import java.util.Scanner;
 
 public class AllTasksInOneL2 {
     public static void main(String[] args) {
 
     }
-    @Test
-    public void getKratnoje4islo() {
-        int n = 20;
-        int[] arr = new int[n];
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите кратное число");
-        int kratnoje = scanner.nextInt();
-        int i = 0;
-        for (i = 0; i < n; i++) {
-            arr[i] = i;
-            if (arr[i] % kratnoje == 0) {
-                System.out.println(arr[i]);
-            }
-        }
 
-    }
-    @Test
-    public int getSmallestNum() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        int a = scanner.nextInt();
-        System.out.print("Введите второе число: ");
-        int b = scanner.nextInt();
+    public int calcSmallestNum(int a, int b) {
+
         if (a < b) {
             return a;
         } else {
             return b;
         }
-
     }
 
+    public void calcAndPrintKratnoje4islo(int kratnoje) {
+        int n = 20;
+        int[] arr = new int[n];
+        for (int i = 0, j = 0; i < n; i++) {
+            if (i % kratnoje == 0) {
+                arr[j++] = i;
+                System.out.println(i);
+            }
+        }
+    }
 }
---
+++ src/test/java/GroupIdQa/AllTaskInOneTest.java
@@ -0,0 +1,21 @@
+package GroupIdQa;
+
+import GroupIdQALevel2.AllTasksInOneL2;
+import org.testng.annotations.Test;
+
+public class AllTaskInOneTest {
+    AllTasksInOneL2 l2 = new AllTasksInOneL2();
+
+    @Test(priority = 1)
+    private void getSmallNumber() {
+        System.out.println("Меньшее число ---> " + l2.calcSmallestNum(5, 2));
+    }
+
+    @Test(priority = 2)
+    private void getKratnoje() {
+        l2.calcAndPrintKratnoje4islo(5);
+
+    }
+
+
+}
